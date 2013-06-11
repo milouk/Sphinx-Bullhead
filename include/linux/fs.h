98e3f0bfaf41934ac391ff7aaba4dfde94464d1c
Al Viro <viro@zeniv.linux.org.uk>
2013-06-11 08:34:36 +0400

    allow the temp files created by open() to be linked to
    O_TMPFILE | O_CREAT => linkat() with AT_SYMLINK_FOLLOW and /proc/self/fd/<n>
    as oldpath (i.e. flink()) will create a link
    O_TMPFILE | O_CREAT | O_EXCL => ENOENT on attempt to link those guys
    
    Change-Id: I5e28485680c3320cd0fccc0ba1bea8b963fca7fe
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Michael <loukerismichalis@gmail.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1777,6 +1777,7 @@ struct super_operations {
 #define I_DIRTY_TIME		(1 << 11)
 #define __I_DIRTY_TIME_EXPIRED	12
 #define I_DIRTY_TIME_EXPIRED	(1 << __I_DIRTY_TIME_EXPIRED)
+#define I_LINKABLE		(1 << 10)
 
 #define I_DIRTY (I_DIRTY_SYNC | I_DIRTY_DATASYNC | I_DIRTY_PAGES)
 #define I_DIRTY_ALL (I_DIRTY | I_DIRTY_TIME)
