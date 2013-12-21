7bed2547fd66357ad5cf76efa89172712faf1913
Tk-Glitch <Ti3noU@gmail.com>
2013-12-21 10:21:12 +0100

    sched: Add sysinterface for GENTLE_FAIR_SLEEPERS
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Conflicts:
    	kernel/ksysfs.c
    	kernel/sched/fair.c

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3632,6 +3632,7 @@ static void check_spread(struct cfs_rq *cfs_rq, struct sched_entity *se)
 }
 
 static unsigned int Lgentle_fair_sleepers = 0;
+
 void relay_gfs(unsigned int gfs)
 {
 	Lgentle_fair_sleepers = gfs;
@@ -3659,7 +3660,7 @@ place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 		 * Halve their sleep time's effect, to allow
 		 * for a gentler effect of sleepers:
 		 */
-		if (sched_feat(GENTLE_FAIR_SLEEPERS))
+		if (Lgentle_fair_sleepers)
 			thresh >>= 1;
 
 		vruntime -= thresh;
