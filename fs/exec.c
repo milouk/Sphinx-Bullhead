c18c932bf04f73850a6928bec67c78a17a66da14
Nai Xia <xianai@nju.edu.cn>
2016-08-15 00:37:08 -0400

    UKSM: initial support
    Patch for 3.10 taken from here: http://kerneldedup.org/en/projects/uksm/download/
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -55,6 +55,7 @@
 #include <linux/pipe_fs_i.h>
 #include <linux/oom.h>
 #include <linux/compat.h>
+#include <linux/ksm.h>
 
 #include <asm/uaccess.h>
 #include <asm/mmu_context.h>
@@ -1166,7 +1167,7 @@ void setup_new_exec(struct linux_binprm * bprm)
 	   group */
 
 	current->self_exec_id++;
-			
+
 	flush_signal_handlers(current, 0);
 }
 EXPORT_SYMBOL(setup_new_exec);
