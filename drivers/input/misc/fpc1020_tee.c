952265debb258b6df87a460ddc5bf2f9dcb11c56
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-09-04 13:56:27 +0200

    soc: qcom: msm_hotplug v2
    Revisited the way big cores are used, as well as how fastlane works, given we
    might be running min cpus at more than 1 on big.LITTLE devices (I use 3 on angler).
    
    - respect msm_thermal core restrictions
    
    - hotplug big cores based on big core load instead of linking 50%
       of big cluster to cpu2 and cpu3 (little)
    
    - checks in sysfs interface to refuse invalid values (return EINVAL).
    
    -------------------------------------------------
    
    SysFs interface (/sys/module/msm_hotplug) :
    
        msm_enabled          : enable/disable hotplugging
                               (rw, 0/1, defaults to 0)
    
        update_rates         : rate for load calculation
                               (rw, defaults to 200)
    
        load_levels          : show/update load levels for little core
                               (rw, format "x y z"
                                  x = 1-4   = core
                                  y = 0-100 = up_threshold
                                  z = 0-100 = down_threshold)
    
        min_cpus_online      : minimum LITTLE cores to keep up at all times
                               (rw, 1-4, defaults to 3 for s810)
    
        max_cpus_online      : maximum LITTLE cores allowed up at all times when screen on
                               (rw, 1-4, defaults to 4 for s810)
    
        max_cpus_online_susp : maximum LITTLE cores allowed up at all times when screen off
                               (rw, 1-4, defaults to 4 for s810)
    
        offline_load         : minimal load to elect a LITTLE core for downing
                               (rw, 0-100, defaults to 0, disabled when 0)
    
        offline_load_big     : average load of all big cores to reach to remove a big core
                               (rw, 0-100, defaults to 20)
    
        online_load_big      : average load of all big cores to reach to add an additional big core
                               (rw, 0-100, defaults to 80)
    
        kick_in_load_big     : average load of all little cores to reach to add first big core
                               (rw, 0-100, defaults to 70)
    
        fast_lane_load       : average load of all running little cores to reach to bring up all
                               big and LITTLE cores
                               (rw, 0/70-100, defaults to 95, disabled when 0)
    
        big_core_up_delay    : delay before brining up big cores in ms
                               (rw, defaults to 0, disabled when 0)
    
        io_is_busy           : consider io as load
                               (rw, 0/1, defaults to 0)
    
        current_load         : show current load (sum of all up cores' load)
                               (ro)
    
        current_cores        : show current core up count
                               (ro, format "big.LITTLE")
    
        version              : show current msm_hotplug version
                               (ro)
    
    -------------------------------------------------
    
    Credits for the msm_hotplug version I based this on to :
    
    - The Linux Foundation
    - Fluxi <linflux@arcor.de>
    - Pranav Vashi <neobuddy89@gmail.com>
    - jollaman999 <admin@jollaman999.com>
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- drivers/input/misc/fpc1020_tee.c
+++ drivers/input/misc/fpc1020_tee.c
@@ -51,9 +51,15 @@
 #endif
 #include <linux/wakelock.h>
 
+
 #ifdef CONFIG_FB
 #include <linux/fb.h>
 #include <linux/notifier.h>
+#endif
+
+#ifdef CONFIG_MSM_HOTPLUG
+#include <linux/msm_hotplug.h>
+#include <linux/workqueue.h>
 #endif
 
 #define FPC1020_RESET_LOW_US 1000
@@ -81,6 +87,10 @@
 #define FPC_TTW_HOLD_TIME 1000
 
 #define KEY_FINGERPRINT 0x2ee
+
+#ifdef CONFIG_MSM_HOTPLUG
+extern void msm_hotplug_resume_timeout(void);
+#endif
 
 static const char * const pctl_names[] = {
 	"fpc1020_spi_active",
@@ -825,6 +835,14 @@ static const struct attribute_group attribute_group = {
 	.attrs = attributes,
 };
 
+#ifdef CONFIG_MSM_HOTPLUG
+static void msm_hotplug_resume_call(struct work_struct *msm_hotplug_resume_call_work)
+{
+	msm_hotplug_resume_timeout();
+}
+static DECLARE_WORK(msm_hotplug_resume_call_work, msm_hotplug_resume_call);
+#endif
+
 static irqreturn_t fpc1020_irq_handler(int irq, void *handle)
 {
 	struct fpc1020_data *fpc1020 = handle;
@@ -836,6 +854,13 @@ static irqreturn_t fpc1020_irq_handler(int irq, void *handle)
 
 	if (!fpc1020->screen_state && fpc1020->wakeup_enabled) {
 		wake_lock_timeout(&fpc1020->ttw_wl, msecs_to_jiffies(FPC_TTW_HOLD_TIME));
+#ifdef CONFIG_MSM_HOTPLUG
+		if (msm_enabled && msm_hotplug_scr_suspended &&
+		   !msm_hotplug_fingerprint_called) {
+			msm_hotplug_fingerprint_called = true;
+			schedule_work(&msm_hotplug_resume_call_work);
+		}
+#endif
 	}
 
 	sysfs_notify(&fpc1020->dev->kobj, NULL, dev_attr_irq.attr.name);
