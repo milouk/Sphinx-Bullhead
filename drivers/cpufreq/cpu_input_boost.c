c9a0b4cdcb22c7b5cdf5e814f32bde242c4321b1
Joe Maples <joe@frap129.org>
2017-08-31 12:47:57 -0400

    cpu_input_boost: Update big.LITTLE masks for msm8992
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -23,11 +23,11 @@
 #define CPU_MASK(cpu) (1U << (cpu))
 
 /*
- * For MSM8996 (big.LITTLE). CPU0 and CPU1 are LITTLE CPUs; CPU2 and CPU3 are
+ * For MSM8992 (big.LITTLE). CPU0-3 are LITTLE CPUs; CPU4-5 are
  * big CPUs.
  */
-#define LITTLE_CPU_MASK (CPU_MASK(0) | CPU_MASK(1))
-#define BIG_CPU_MASK    (CPU_MASK(2) | CPU_MASK(3))
+#define LITTLE_CPU_MASK (CPU_MASK(0) | CPU_MASK(1) | CPU_MASK(2) | CPU_MASK(3))
+#define BIG_CPU_MASK (CPU_MASK(4) | CPU_MASK(5))
 
 /* Fingerprint sensor input key */
 #define FINGERPRINT_KEY 0x2ee
