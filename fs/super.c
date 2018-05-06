866dc36a8b38bc84047ed21ffbc7a2ba52506b88
Michael <loukerismichalis@gmail.com>
2018-05-06 14:07:02 +0200

    fixed compilation

--- fs/super.c
+++ fs/super.c
@@ -157,19 +157,6 @@ static struct super_block *alloc_super(struct file_system_type *type, int flags)
 		if (security_sb_alloc(s))
 			goto out_free_sb;
 
-#ifdef CONFIG_SMP
-		s->s_files = alloc_percpu(struct list_head);
-		if (!s->s_files)
-			goto err_out;
-		else {
-			int i;
-
-			for_each_possible_cpu(i)
-				INIT_LIST_HEAD(per_cpu_ptr(s->s_files, i));
-		}
-#else
-		INIT_LIST_HEAD(&s->s_files);
-#endif
 
 		if (init_sb_writers(s, type))
 			goto err_out;
