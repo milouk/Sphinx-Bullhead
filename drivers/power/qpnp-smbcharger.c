e4c97d21bdd951512ddd3b1f1ce95188299d11da
Laster K <officiallazerl0rd@gmail.com>
2018-05-26 11:09:09 +0100

    misc: add fastchg driver
    Control forced fast charge via /sys/kernel/fast_charge/force_fast_charge (0 or 1).
    Signed-off-by: Laster K. <officiallazerl0rd@gmail.com>

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -39,6 +39,10 @@
 #include <linux/msm_bcl.h>
 #include <linux/ktime.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
+
 /* Mask/Bit helpers */
 #define _SMB_MASK(BITS, POS) \
 	((unsigned char)(((1 << (BITS)) - 1) << (POS)))
@@ -951,7 +955,7 @@ static const int usb_current_table[] = {
 	2300,
 	2400,
 	2500,
-	3000
+	3000,
 };
 
 static const int dc_current_table[] = {
@@ -981,13 +985,17 @@ static const int dc_current_table[] = {
 	1950,
 	1970,
 	2000,
+        2050,
+        2100,
+        2300,
+        2400,
 };
 
 static const int fcc_comp_table[] = {
 	250,
-	700,
-	900,
+	1100,
 	1200,
+	1500,
 };
 
 static int calc_thermal_limited_current(struct smbchg_chip *chip,
@@ -1422,7 +1430,11 @@ static int smbchg_set_usb_current_max(struct smbchg_chip *chip,
 					USBIN_LIMITED_MODE | USB51_500MA);
 		chip->usb_max_current_ma = 500;
 	}
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if ((force_fast_charge > 0 && current_ma == CURRENT_500_MA) || current_ma == CURRENT_900_MA) {
+#else
 	if (current_ma == CURRENT_900_MA) {
+#endif
 		rc = smbchg_sec_masked_write(chip,
 					chip->usb_chgpth_base + CHGPTH_CFG,
 					CFG_USB_2_3_SEL_BIT, CFG_USB_3);
