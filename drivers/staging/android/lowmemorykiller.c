dd8be2c3bc95b1573756766733504c7e9eff76de
Albert I <krascgq@outlook.co.id>
2018-06-15 15:08:49 +0800

    Revert "lowmemorykiller: Account for unevictable pages in vmpressure notifier"
    This reverts commit e741e0b84149d2edd507c881465d20c88e3b0669.
    Pick from LineageOS/android_kernel_qcom_msm8996 instead.
    
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -130,7 +130,6 @@ static int lmk_vmpressure_notifier(struct notifier_block *nb,
 	if (pressure >= 95) {
 		other_file = global_page_state(NR_FILE_PAGES) -
 			global_page_state(NR_SHMEM) -
-			global_page_state(NR_UNEVICTABLE) -
 			total_swapcache_pages();
 		other_free = global_page_state(NR_FREE_PAGES);
 
@@ -144,7 +143,6 @@ static int lmk_vmpressure_notifier(struct notifier_block *nb,
 
 		other_file = global_page_state(NR_FILE_PAGES) -
 			global_page_state(NR_SHMEM) -
-			global_page_state(NR_UNEVICTABLE) -
 			total_swapcache_pages();
 
 		other_free = global_page_state(NR_FREE_PAGES);
